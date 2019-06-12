966b4de4a8ba7965f2676378d8701dbe6806d1e5
ClauGarfiasMx <apps@claudiagarfias.works>
2019-06-12 16:16:39 -0500

    Updates

---
+++ precache-manifest.8206d706babd0e86604d7b74763be938.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71c79c39f21fc02bf8eff9bdd6045940",
+    "url": "/socialbook/index.html"
+  },
+  {
+    "revision": "594983ee673fb854e56a",
+    "url": "/socialbook/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/socialbook/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "594983ee673fb854e56a",
+    "url": "/socialbook/static/js/main.cd94db84.chunk.js"
+  },
+  {
+    "revision": "45d55a2ea6bcd97ed85a",
+    "url": "/socialbook/static/js/runtime~main.650fe429.js"
+  }
+]);
